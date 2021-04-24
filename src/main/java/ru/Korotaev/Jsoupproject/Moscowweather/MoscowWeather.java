0f973052ffd753fc47fcb66ad85fef581cd896e9
serezakorotaev <74141316+serezakorotaev@users.noreply.github.com>
2021-04-24 20:48:41 +0300

    changed in MoscowWeather class code and i tried new nested loop without iteratorCount as in other classes. The next will try reduce double code

--- src/main/java/ru/Korotaev/Jsoupproject/Moscowweather/MoscowWeather.java
+++ src/main/java/ru/Korotaev/Jsoupproject/Moscowweather/MoscowWeather.java
@@ -18,14 +18,23 @@ public class MoscowWeather implements GetPage {
         MoscowWeather moscowWeather = new MoscowWeather();
         Document page = moscowWeather.getPage("https://weather.com");
         Element tableWth = page.select("div[id=WxuDailyWeatherCard-main-bb1a17e7-dc20-421a-b1b8-c117308c6626]").first();
-        Elements weatherToday = tableWth.select("a[class=Column--innerWrapper--3K14X  Button--default--2yeqQ]");
-//        Elements names = tableWth.select("li[class=Column--column--2bRa6]");
-        Elements temperatureAndHumidity = weatherToday.select("div");
-        //
-        for(Element weathToday : weatherToday){
-            String weathTodayString = weathToday.select("span[class=Ellipsis--ellipsis--lfjoB]").text();
-            System.out.println(weathTodayString);
-            System.out.println(temperatureAndHumidity.text());
+
+        Elements allWeatherTodayWithAllInformation = tableWth.select("li");
+        for (Element element : allWeatherTodayWithAllInformation) {
+            //find necessary link in different links with temperature and get all information from it is link
+            Elements weatherToday = element.select("a[class=Column--innerWrapper--3K14X  Button--default--2yeqQ]");
+            Elements temperatureAndHumidity = weatherToday.select("div");
+            // Break it into smaller parts with week's day and day's temperature
+            for (Element weathToday : weatherToday) {
+
+                String weathTodayString = weathToday.select("span[class=Ellipsis--ellipsis--lfjoB]").text();
+                System.out.println(weathTodayString);
+                for (Element temperatureInDay : temperatureAndHumidity) {
+                    String temperatureHigh = temperatureInDay.select("div[data-testid=SegmentHighTemp]").text();
+                    String temperatureLow = temperatureInDay.select("div[data-testid=SegmentLowTemp]").text();
+                    System.out.println(temperatureHigh + temperatureLow);
+                }
+            }
         }
     }
 }
